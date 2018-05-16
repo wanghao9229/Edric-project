f526ea209043c5b19ae5eed515a2b6c9260611e0
Edric Wang <wanghao339229@gmail.com>
2018-05-16 18:09:30 +1200

    16.05
    model

--- www/orm.py
+++ www/orm.py
@@ -82,3 +82,21 @@ class Model(dict, metaclass=ModelMetaclass):
                 logging.debug('using default value for %s: %s' % (key, str(value)))
                 setattr(self, key, value)
         return value
+
+class Field(object):
+
+    def __init__(self, name, column_type, primary_key, default):
+        self.name = name
+        self.column_type = column_type
+        self.primary_key = primary_key
+        self.default = default
+
+    def __str__(self):
+        return '<%s, %s:%s>' % (self.__class__,__name__, self.column_type, self.name)
+
+
+class StringField(Field):
+    def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
+        super().__init__(name, ddl, primary_key, default)
+
+        
